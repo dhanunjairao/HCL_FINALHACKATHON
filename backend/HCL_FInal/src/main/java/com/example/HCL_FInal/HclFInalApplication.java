2375b2ae6b97412d43b5c73df89d6e4765a4b914
dhanunjairao <dhanunjairao_ari@srmap.edu.in>
2026-05-25 13:24:39 +0530

    Initial commit

---
+++ backend/HCL_FInal/src/main/java/com/example/HCL_FInal/HclFInalApplication.java
@@ -0,0 +1,13 @@
+package com.example.HCL_FInal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HclFInalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HclFInalApplication.class, args);
+	}
+
+}
---
+++ backend/HCL_FInal/src/test/java/com/example/HCL_FInal/HclFInalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HCL_FInal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HclFInalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
